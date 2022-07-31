8f7c9983eb469d7b9c8c9bc4223db35ecae7aac6
Lightmann <lightmanns11@gmail.com>
2022-07-31 18:39:15 -0400

    Fix indentation

--- README.md
+++ README.md
@@ -3,37 +3,37 @@
 
 ## Dependencies
 ### General
-    sudo apt install curl libssl1.1 openssh-client rsync sudo unzip
+	sudo apt install curl libssl1.1 openssh-client rsync sudo unzip
 
 ### img4tool
-    mkdir img4tool && cd img4tool
+	mkdir img4tool && cd img4tool
 
-    curl -L https://github.com/tihmstar/img4tool/releases/latest/download/buildroot_ubuntu-latest.zip --output img4tool-latest.zip
+	curl -L https://github.com/tihmstar/img4tool/releases/latest/download/buildroot_ubuntu-latest.zip --output img4tool-latest.zip
 
-    unzip img4tool-latest.zip
+	unzip img4tool-latest.zip
 
-    sudo cp buildroot_ubuntu-latest/usr/local/bin/img4tool /usr/local/bin/img4tool
+	sudo cp buildroot_ubuntu-latest/usr/local/bin/img4tool /usr/local/bin/img4tool
 
-    sudo chmod +x /usr/local/bin/img4tool
+	sudo chmod +x /usr/local/bin/img4tool
 
-    sudo cp -R buildroot_ubuntu-latest/usr/local/include/img4tool /usr/local/include
+	sudo cp -R buildroot_ubuntu-latest/usr/local/include/img4tool /usr/local/include
 
-    cd .. && rm -r img4tool
+	cd .. && rm -r img4tool
 
 ### On your idevice:
 Install `openssh` from your package manager or via cli with
 
-    sudo apt install openssh
+	sudo apt install openssh
 
 ## Before running the script
-    chmod +x deverser-linux.sh
+	chmod +x deverser-linux.sh
 
 Ensure that your idevice is connected to the same WiFi network as your pc and test the ssh connection by running `ssh root@your_device_ip_address` and entering your root password when prompted. Once you've confirmed that you can access your device, type `exit`.
 
 **Note**: the default root password is "alpine" and should be changed if you haven't changed it already by typing `su`, logging in with "alpine," typing `passwd`, and following the prompts.
 
 ## Running the script
-    sudo ./deverser-linux.sh
+	sudo ./deverser-linux.sh
 
 If you've done everything correctly, you should see a .shsh2 file in your current working directory!
 
--- deverser-linux.sh
+++ deverser-linux.sh
@@ -4,52 +4,52 @@ echo "[!] Welcome to Déverser, a simple script to dump onboard SHSH (Blobs) wit
 echo "[!] The blobs dumped with this script can be used with futurerestore at a later date (depending on SEP compatibility)."
 
 if ! [[ -x $(command -v curl) ]]; then
-    echo "[*] ERROR: dependency check failed -- curl could not be found."
-    exit 1
+	echo "[*] ERROR: dependency check failed -- curl could not be found."
+	exit 1
 elif ! [[ -x $(command -v rsync) ]]; then
-    echo "[*] ERROR: dependency check failed -- rsync could not be found."
-    exit 1
+	echo "[*] ERROR: dependency check failed -- rsync could not be found."
+	exit 1
 elif ! [[ -x $(command -v ssh) ]]; then
-    echo "[*] ERROR: dependency check failed -- openssh-client could not be found."
-    exit 1
+	echo "[*] ERROR: dependency check failed -- openssh-client could not be found."
+	exit 1
 elif ! [[ -x $(command -v sudo) ]]; then
-    echo "[*] ERROR: dependency check failed -- sudo could not be found."
-    exit 1
+	echo "[*] ERROR: dependency check failed -- sudo could not be found."
+	exit 1
 elif ! [[ -x $(command -v unzip) ]]; then
-    echo "[*] ERROR: dependency check failed -- unzip could not be found."
-    exit 1
+	echo "[*] ERROR: dependency check failed -- unzip could not be found."
+	exit 1
 fi
 
 if ! [[ -f "/usr/local/bin/img4tool" ]]; then
-    while true; do
-        echo "[*] ERROR: dependency check failed -- img4tool could not be found."
-        read -p "[#] Would you like Déverser to download and install img4tool for you? (y/n)" consent
-        if [[ ${consent,,} == "y" || ${consent,,} == "yes" ]]; then
-            echo "[!] Downloading latest img4tool from Tihmstar's repo..."
-            mkdir img4tool && cd img4tool
-            curl -L https://github.com/tihmstar/img4tool/releases/latest/download/buildroot_ubuntu-latest.zip --output img4tool-latest.zip
-            unzip -q img4tool-latest.zip
-            echo "[#] These next few steps require sudo; please enter your root password if prompted:"
-            sudo cp buildroot_ubuntu-latest/usr/local/bin/img4tool /usr/local/bin/img4tool
-            sudo chmod +x /usr/local/bin/img4tool
-            sudo cp -R buildroot_ubuntu-latest/usr/local/include/img4tool /usr/local/include
-            cd .. && rm -rf img4tool/
-            if [[ -f "/usr/local/bin/img4tool" ]]; then
-                echo "[!] Successfully installed img4tool!"
-                break
-            else
-                echo "[*] ERROR: Failed to install img4tool!"
-                exit 1
-            fi
-        elif [[ ${consent,,} == "n" || ${consent,,} == "no" ]]; then
-            echo "[!] img4tool is required for this script. Please either run this script again and respond 'y' to the install prompt or install it yourself manually (see README.md)."
-            exit 1
-        fi
-    done
+	while true; do
+		echo "[*] ERROR: dependency check failed -- img4tool could not be found."
+		read -p "[#] Would you like Déverser to download and install img4tool for you? (y/n)" consent
+		if [[ ${consent,,} == "y" || ${consent,,} == "yes" ]]; then
+			echo "[!] Downloading latest img4tool from Tihmstar's repo..."
+			mkdir img4tool && cd img4tool
+			curl -L https://github.com/tihmstar/img4tool/releases/latest/download/buildroot_ubuntu-latest.zip --output img4tool-latest.zip
+			unzip -q img4tool-latest.zip
+			echo "[#] These next few steps require sudo; please enter your root password if prompted:"
+			sudo cp buildroot_ubuntu-latest/usr/local/bin/img4tool /usr/local/bin/img4tool
+			sudo chmod +x /usr/local/bin/img4tool
+			sudo cp -R buildroot_ubuntu-latest/usr/local/include/img4tool /usr/local/include
+			cd .. && rm -rf img4tool/
+			if [[ -f "/usr/local/bin/img4tool" ]]; then
+				echo "[!] Successfully installed img4tool!"
+				break
+			else
+				echo "[*] ERROR: Failed to install img4tool!"
+				exit 1
+			fi
+		elif [[ ${consent,,} == "n" || ${consent,,} == "no" ]]; then
+			echo "[!] img4tool is required for this script. Please either run this script again and respond 'y' to the install prompt or install it yourself manually (see README.md)."
+			exit 1
+		fi
+	done
 fi
 
 if [[ -f "dump.raw" ]]; then
-    rm -rf dump.raw
+	rm -rf dump.raw
 fi
 
 read -p "[#] Please enter your device's IP address (which can be found in WiFi settings):" ip
@@ -62,16 +62,16 @@ echo "[#] Please enter the device's root password again (default is 'alpine'):"
 rsync -az --remove-source-files -e 'ssh -p 22' root@${ip}:dump.raw dump.raw
 
 if [[ -f "dump.raw" ]]; then
-    echo "[!] Successfully moved dump.raw to this machine and preparing to convert to SHSH..."
+	echo "[!] Successfully moved dump.raw to this machine and preparing to convert to SHSH..."
 else
-    echo "[#] ERROR: Failed to to move 'dump.raw' from device to this machine!"
-    exit 2
+	echo "[#] ERROR: Failed to to move 'dump.raw' from device to this machine!"
+	exit 2
 fi
 
 img4tool --convert -s dumped.shsh dump.raw &> /dev/null
 if img4tool -s dumped.shsh | grep -q 'failed'; then
-    echo "[*] ERROR: Failed to create SHSH from 'dump.raw'!"
-    exit 3
+	echo "[*] ERROR: Failed to create SHSH from 'dump.raw'!"
+	exit 3
 fi
 
 ECID=$(img4tool -s dumped.shsh | grep "ECID" | cut -c13-)
@@ -79,12 +79,12 @@ mv dumped.shsh $ECID.dumped.shsh # Allows multiple devices to be dumped as each 
 GENERATOR=$(cat $ECID.dumped.shsh | grep "<string>0x" | cut -c10-27)
 
 if [[ -f "$ECID.dumped.shsh" ]]; then
-    echo "[!] Successfully dumped SHSH!"
-    echo "[!] The generator for said SHSH is: $GENERATOR"
-    echo "[!] Note: the string of numbers in the filename is your device's ECID."
+	echo "[!] Successfully dumped SHSH!"
+	echo "[!] The generator for said SHSH is: $GENERATOR"
+	echo "[!] Note: the string of numbers in the filename is your device's ECID."
 else
-    echo "[*] ERROR: Failed to dump SHSH!"
-    exit 4
+	echo "[*] ERROR: Failed to dump SHSH!"
+	exit 4
 fi
 
 echo "[@] Originally written by Matty (@moski_dev), made linux compatible by IlanM, and refined by Lightmann."
